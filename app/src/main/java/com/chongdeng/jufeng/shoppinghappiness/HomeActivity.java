e2e3ad4080a8f71ac70646101fa3ef19691c5a63
chong <fqyyang@163.com>
2017-09-03 12:46:06 -0700

    add botoom navigation ui and logic

--- app/src/main/java/com/chongdeng/jufeng/shoppinghappiness/HomeActivity.java
+++ app/src/main/java/com/chongdeng/jufeng/shoppinghappiness/HomeActivity.java
@@ -1,23 +1,113 @@
 package com.chongdeng.jufeng.shoppinghappiness;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
+import android.view.MenuItem;
+
+import fragments.BottomNavFragment;
+import helper.BottomNavigationViewHelper;
+
 
 /**
- * Created by fqyang on 8/31/2017.
+ * Created by chong on 8/31/2017.
  */
 
 public class HomeActivity extends AppCompatActivity {
+    private static final String SELECTED_ITEM = "arg_selected_item";
+
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
+
+        //disable shift mode
+        BottomNavigationViewHelper.disableShiftMode(mBottomNav);
+
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mBottomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        MenuItem homeItem = mBottomNav.getMenu().getItem(0);
+        if (mSelectedItem != homeItem.getItemId()) {
+            // select home item
+            selectFragment(homeItem);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                frag = BottomNavFragment.newInstance(getString(R.string.text_home),
+                        getColorFromRes(R.color.color_home));
+                break;
+            case R.id.action_merchandise:
+                frag = BottomNavFragment.newInstance(getString(R.string.text_notifications),
+                        getColorFromRes(R.color.color_home));
+                break;
+            case R.id.action_favorites:
+                frag = BottomNavFragment.newInstance(getString(R.string.text_notifications),
+                        getColorFromRes(R.color.color_home));
+                break;
+            case R.id.action_me:
+                frag = BottomNavFragment.newInstance(getString(R.string.text_search),
+                        getColorFromRes(R.color.color_home));
+                break;
+        }
+
+        // update selected item
+        mSelectedItem = item.getItemId();
+
+        // uncheck the other items.
+        for (int i = 0; i< mBottomNav.getMenu().size(); i++) {
+            MenuItem menuItem = mBottomNav.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
+    }
+
+    private int getColorFromRes(@ColorRes int resId) {
+        return ContextCompat.getColor(this, resId);
     }
 }
-
-
